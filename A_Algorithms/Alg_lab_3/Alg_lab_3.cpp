a2b50a15b9e07d00a4f359528dbfe613e220d127
VlasMaru <118015689+VlasMaru@users.noreply.github.com>
2023-09-24 22:49:58 +0200

    Added lab_3 and lab_4

---
+++ A_Algorithms/Alg_lab_3/Alg_lab_3.cpp
@@ -0,0 +1,46 @@
+﻿/* Лаба №3 "Задача о простых множителях"
+На вход дается одно число х, нужно вывести все числа от 1 до х, удовлетворяющие условию :
+3 ^ K * 5 ^ L * 7 ^ M = xi
+
+где K, L, M - натуральные числа или могут быть равны 0.
+
+	for (int k = 1; k < ; k++)
+
+
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	double print = 0;
+	int ex = 0;
+	int add = 1;
+	bool flag = false;
+	std::cin >> print;
+
+	int k_range = pow(print, 1 / 3.0) + 2;
+	int l_range = pow(print, 1 / 5.0) + 2;
+	int m_range = pow(print, 1 / 7.0) + 2;
+	
+		for (int k = 0; k < k_range; k++)
+		{
+				for (int l = 0; l < l_range; l++)
+				{
+					for (int m = 0; m < m_range; m++)
+					{
+						ex = pow(3, k) + pow(5, l) + pow(7, m);
+
+						if (ex <= print) { std::cout << ex << ":  K = " << k << " L = " << l << " M = " << m << std::endl; add++; }
+						
+					}
+				}
+		}
+
+
+
+	
+
+}
+
---
+++ A_Algorithms/Alg_lab_4/Alg_lab_4.cpp
@@ -0,0 +1,80 @@
+﻿/*
+Дана последовательность чисел. Отсортировать и вывести последовательность чисел, определённым методом.
+№4 Сортировка методом прочесывания.
+*/
+
+#include <iostream>
+#include <vector>
+
+
+void PrintVec (std::vector <int> vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << "  ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	int mas_size = 0; 
+	std::cout << "Enter your massive size: " << std::endl;
+	std::cin >> mas_size;
+	std::vector <int> mas;
+	int tempy(0);
+	std::cout << "Enter your massive elements: " << std::endl;
+	for (int x = 0; x < mas_size; x++)
+	{
+		std::cin >> tempy;
+		mas.push_back(tempy);
+	}
+	PrintVec(mas);
+	std::cout << std::endl;
+
+	int step = mas_size - 1;
+
+	for (int x = 0; x < mas_size ; x++)
+	{
+		if (step >= 1)
+		{
+			for (int i = 0; (i + step) < mas_size; i++)
+			{
+				if (mas[i] > mas[i + step]) { std::swap(mas[i], mas[i + step]); PrintVec(mas); }
+			}
+			step--;
+		}
+		
+
+	}
+	
+	
+	std::cout << "Sorted massive: " << std::endl;
+	PrintVec(mas);
+}
+
+
+
+/*
+* 
+* 
+* 
+* 
+* int step = 1;
+	for (int x = 0; x < mas_size; x++)
+	{
+		for (int i = 0; i < step; i++)
+		{
+		  if (mas[i] > mas[mas_size - step]) { std::swap(mas[i], mas[mas_size - step]); PrintVec(mas); } 
+		}
+		if ((step + 1) < mas_size) { step++; }
+		else if ((step + 1) >= mas_size) { break;}
+	}
+
+
+
+
+
+
+	}*/
+
