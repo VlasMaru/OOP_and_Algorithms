b59e63880c2d4d1b8cd25c22830527bc504fc34c
VlasMaru <118015689+VlasMaru@users.noreply.github.com>
2023-10-09 00:03:50 +0200

    Lab_7

--- A_Algorithms/Alg_lab_7/Alg_lab_7.cpp
+++ A_Algorithms/Alg_lab_7/Alg_lab_7.cpp
@@ -21,7 +21,7 @@ int main()
 	std::cout << "Enter your massive size: " << std::endl;
 	std::cin >> mas_size;
 	std::vector <int> mas{};
-	std::vector <int> fib{ 1, 4, 10, 23, 57, 123, 301, 701, 1750};
+	std::vector <int> fib{ 0, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 	int tempy(0);
 	std::cout << "Enter your massive elements: " << std::endl;
 	for (int x = 0; x < mas_size; x++)
@@ -31,29 +31,42 @@ int main()
 	}
 	PrintVec(mas);
 	std::cout << std::endl;
-	int b{ 0 };
-	while (fib[b] <= mas_size)
+	int b{ 1 };
+	while (fib[b]+2 <= mas_size)
 	{
 		b++;
 	}
-	int start_step = fib[b-1];
+	int start_step = fib[b - 1];
 	////
-	int n = 1;
-	for (int x = 0; x < mas_size; x++)
+	int n = 2;
+	while (b-n>=0)
 	{
-		if (n <= b)
-		{
-			for (int i = 0; (i + b-1) < mas_size; i++)
+			for (int i = 0; (i + start_step) < mas_size; i++)
 			{
-				if (mas[i] > mas[i + start_step]) { std::swap(mas[i], mas[i + start_step]); PrintVec(mas); }
+				if (mas[i] > mas[i + start_step])
+				{
+					std::swap(mas[i], mas[i + start_step]);
+					PrintVec(mas);
+					int g = i;
+
+					while (g > 0)
+					{
+						if (mas[g] < mas[g-1])
+						{
+							std::swap(mas[g], mas[g-1]); PrintVec(mas);
+							g--;
+						}
+						else{ g--;}
+						
+					}
+
+					
+				}
 			}
-			start_step = fib[b - n];
-			n++;
-		}
-
-
+		start_step = fib[b - n];
+		n++;
 	}
-
+		
 
 	////
 
---
+++ A_Algorithms/Alg_lab_8/Alg_lab_8.cpp
@@ -0,0 +1,74 @@
+﻿// Метод сортировки: поразрядная
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+void PrintVec(std::vector <int> vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << "  ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	int mas_size = 0;
+	std::cout << "Enter your massive size: " << std::endl;
+	std::cin >> mas_size;
+	std::vector <int> mas{};
+	int tempy(0);
+	std::cout << "Enter your massive elements: " << std::endl;
+	int max{ 0 };
+	for (int x = 0; x < mas_size; x++)
+	{
+		std::cin >> tempy;
+		if (tempy > max) { max = tempy;}
+		mas.push_back(tempy);
+	}
+	PrintVec(mas);
+	std::cout << std::endl;
+
+	////
+	std::string max_string = std::to_string(max);
+	int max_range = max_string.length();
+
+	//std::vector <int> mas_raz{};
+	int n = max_range;
+
+
+	while (n <= 0)
+	{
+		for (int x = 0; x < mas_size-1; x++)
+		{
+			if ((mas[x] / pow(10,n) > (mas[x+1] / pow(10, n))))
+			{
+				std::swap(mas[x], mas[x + 1]);
+				PrintVec(mas);
+				int tempp = x;
+				while (tempp != 0)
+				{
+					if ((mas[tempp] / pow(10, n) > (mas[x] / pow(10, n))))
+					{
+						std::swap(mas[x], mas[x + 1]);
+						PrintVec(mas);
+						x--;
+					}
+					tempp--;
+				}
+			}
+		}
+		n--;
+	}
+	
+
+	////
+
+
+	std::cout << "Sorted massive: " << std::endl;
+	PrintVec(mas);
+}
