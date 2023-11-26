8be6e84cb33701bb478285e6cd8b2ba57d5ea92d
VlasMaru <118015689+VlasMaru@users.noreply.github.com>
2023-11-26 23:53:37 +0200

    --

---
+++ A_Algorithms/Alg_lab_10/Alg_lab_10.cpp
@@ -0,0 +1,120 @@
+﻿
+#include <iostream>
+#include <vector>
+
+
+
+
+void MergSort(std::vector <int>& mas, int mas_size, int from,int to)
+{
+	if (mas_size == 2)
+	{
+		if ((mas[from] > mas[to])) { std::swap(mas[from], mas[to]); }
+	}
+	if ((mas_size != 2)&&(mas_size != 1))
+	{
+		int l_size = mas_size / 2;
+		int r_size = mas_size - l_size;
+		int l_ind = from;
+		int r_ind = from + l_size;
+		MergSort(mas, l_size, l_ind, l_ind + l_size-1);
+		MergSort(mas, r_size, r_ind, to);
+		std::vector <int> sup_mas;
+		//for (int v = 0; v < mas_size; v++) { sup_mas.push_back(-873); }
+		int counter = 0;
+		int counter_l = l_ind;
+		int counter_r = r_ind;
+		int l_end = r_ind;
+		int r_end = mas_size + from;
+		while (counter != mas_size)
+		{
+			if (counter_l == l_end)
+			{
+				sup_mas.push_back(mas[counter_r]);
+				counter++;
+				counter_r++;
+			}
+
+			else if (counter_r == r_end)
+			{
+				sup_mas.push_back(mas[counter_l]);
+				counter++;
+				counter_l++;
+			}
+
+			else if ((counter_r != r_end) && (counter_l != l_end))
+			{
+				if (mas[counter_l] <= mas[counter_r])
+				{
+					sup_mas.push_back(mas[counter_l]);
+					counter++;
+					counter_l++;
+				}
+
+				else if (mas[counter_l] > mas[counter_r])
+				{
+					sup_mas.push_back(mas[counter_r]);
+					counter++;
+					counter_r++;
+				}
+			}
+			
+		}
+
+		for (int c = 0; c < mas_size; c++)
+		{
+			mas[c+from] = sup_mas[c];
+		}
+
+		/*
+		for (int x = 0; x < l_size; x++)
+		{
+			for (int y = 0; y < r_size; y++)
+			{
+				if (mas[l_ind+x] < mas[r_ind+y]) { std::swap(mas[l_ind + x],mas[r_ind + y]);}
+			}
+			
+		}*/
+	}
+}
+void PrintVec(std::vector <int> vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << "  ";
+	}
+	std::cout << std::endl;
+}
+
+
+
+int main()
+{
+	int mas_size = 0;
+	std::cout << "Enter your massive size: " << std::endl;
+	std::cin >> mas_size;
+	std::vector <int> mas{};
+	int tempy(0);
+	std::cout << "Enter your massive elements: " << std::endl;
+	for (int x = 0; x < mas_size; x++)
+	{
+		std::cin >> tempy;
+		mas.push_back(tempy);
+	}
+	std::cout << "Unsorted massive: " << std::endl;
+	PrintVec(mas);
+	std::cout << std::endl;
+
+	////
+
+	MergSort(mas, mas_size, 0, mas_size - 1);
+
+
+	////
+
+	std::cout << "Sorted massive: " << std::endl;
+	PrintVec(mas);
+
+
+
+}
---
+++ A_Algorithms/Alg_lab_11/Alg_lab_11.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <vector>
+
+void PrintVec(std::vector <int> vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << "  ";
+	}
+	std::cout << std::endl;
+}
+/*void QSort(std::vector <int>& mas, int mas_size, int from, int to)
+{
+	if (mas_size != 1)
+	{
+		int key = mas[from];
+		std::vector <int> temp{};
+		temp.push_back(key);
+		int l_size = 0;
+		int r_size = 0;
+		for (int x = 0; x < mas_size; x++)
+		{
+			if ((mas[x] >= key) && (x != from))
+			{
+				temp.push_back(mas[x]);
+				r_size++;
+			}
+			else if ((mas[x] < key) && (x != from))
+			{
+				temp.insert(temp.begin(), mas[x]);
+				l_size++;
+			}
+
+		}
+		for (int c = 0; c < mas_size; c++) { mas[c] = temp[c]; }
+		QSort(mas, l_size, from, from + l_size);
+		QSort(mas, r_size, from + l_size, to);
+
+	}
+
+
+	//////
+
+	if (mas_size >= 2)
+	{
+		int key = mas[0];
+		std::vector <int> left_side;
+		std::vector <int> right_side;
+		for (int x = 0; x < mas_size; x++)
+		{
+			if (mas[x] <= key) { left_side.push_back(mas[x]); }
+			else if (mas[x] > key) { right_side.push_back(mas[x]);}
+		}
+		if (left_side.size() != 0) { QSort(left_side, left_side.size()); }
+		if (right_side.size() != 0) { QSort(right_side, right_side.size()); }
+		mas.clear();
+		for (int c = 0; c < left_side.size(); c++) { mas.push_back(left_side[c]); }
+		for (int g = 0; g < right_side.size(); g++) { mas.push_back(right_side[g]); }
+	}
+}*/
+
+int NotMax(std::vector <int>& mas,int mas_size)
+{
+	for (int y = 1; y < mas_size; y++) { if (mas[y] > mas[0]) { return mas[0]; } }
+	return mas[1];
+}
+
+void QSort(std::vector <int>& mas, int mas_size)
+{
+	if (mas_size >= 2)
+	{
+		int key = NotMax(mas,mas_size);
+		std::vector <int> left_side;
+		std::vector <int> right_side;
+		std::vector <int> same;
+		for (int x = 0; x < mas_size; x++)
+		{
+			if (mas[x] <= key) { left_side.push_back(mas[x]); }
+			else if (mas[x] > key) { right_side.push_back(mas[x]);}
+			else if (mas[x] == key) {same.push_back(mas[x]);}
+		}
+		if ((left_side.size() != 0) && (right_side.size() != 0)) 
+		{ QSort(left_side, left_side.size());
+		QSort(right_side, right_side.size());
+		}
+	
+		mas.clear();
+		for (int c = 0; c < left_side.size(); c++) { mas.push_back(left_side[c]); }
+		for (int g = 0; g < right_side.size(); g++) { mas.push_back(right_side[g]); }
+	}
+}
+	
+
+
+int main()
+{
+	int mas_size = 0;
+	std::cout << "Enter your massive size: " << std::endl;
+	std::cin >> mas_size;
+	std::vector <int> mas{};
+	int tempy(0);
+	std::cout << "Enter your massive elements: " << std::endl;
+	for (int x = 0; x < mas_size; x++)
+	{
+		std::cin >> tempy;
+		mas.push_back(tempy);
+	}
+	std::cout << "Unsorted massive: " << std::endl;
+	PrintVec(mas);
+	std::cout << std::endl;
+
+	////
+
+	QSort(mas, mas_size);
+
+
+	////
+
+	std::cout << "Sorted massive: " << std::endl;
+	PrintVec(mas);
+
+
+
+}
